0ca8ca692df5cb30dc06428dae7a72cf946af042
Kim heonil <iamheonil@gmail.com>
2020-11-09 23:13:50 +0900

    Swift 버튼 만들기 이벤트는 천천히 ㅠ.ㅠ

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! Oh My God")
-            .padding()
+        VStack {
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                
+            }
+            VStack {
+                Text("Hello, world! Oh My God")
+                    .padding(-100)
+            }
+        }
     }
 }
 
