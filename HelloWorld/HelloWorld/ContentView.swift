6a0866e939946cc478d9b8628cbc1f2c47641a16
Kim heonil <iamheonil@gmail.com>
2021-09-18 11:32:59 +0900

    서위프트 변수 선언해서 화면에 출력해보기

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    var numberOfPlanets : Int = 8
+    var diameterOfEarth : Float = 23859.92
+    
     var body: some View {
+        
         VStack {
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
                 
             }
             VStack {
-                Text("Hello, world! Oh My God")
-                    .padding(-100)
+                Text("Hello, world!! \(numberOfPlanets), \(diameterOfEarth) ").padding(-50)
             }
         }
+        
+
+        
+        
     }
 }
 
